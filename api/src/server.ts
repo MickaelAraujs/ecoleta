39ba958279ef3aa9874f5e687456e12f27fbd1bc
Mickael Araujo <araujomickael18@gmail.com>
2020-06-02 17:31:30 -0300

    create routes file

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,14 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-    return response.json([
-        {
-            name: 'Mickael',
-            email: 'mickael@mickael.com'
-        },
-    ])
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)

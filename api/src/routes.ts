3219c0c27e47b2355f239694c6d109d2aca94b0d
Mickael Araujo <araujomickael18@gmail.com>
2020-06-03 18:23:30 -0300

    Points and Items Controllers

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 
+import knex from './database/connection'
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
 const routes = express.Router()
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello! :)' })
-})
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
+
+routes.get('/items', itemsController.index)
+
+routes.post('/points', pointsController.create)
 
 export default routes

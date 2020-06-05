4fead09310cea35dbeaf5c94ec737026c41d71e4
Mickael Araujo <araujomickael18@gmail.com>
2020-06-05 18:16:09 -0300

    home page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { FiLogIn } from 'react-icons/fi'
+
+import logo from '../../assets/logo.svg'
+import './styles.css'
 
 const Home = () => {
   return (
-    <h1>home</h1>
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Ecoleta - Seu marketplace de coleta de resíduos"/>
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+          <a href="/cadastro">
+            <span>
+              <FiLogIn />
+            </span>
+
+            <strong>
+              Cadastre um ponto de coleta
+            </strong>
+          </a>
+        </main>
+      </div>
+    </div>
   )
 }
 

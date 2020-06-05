581ab352f3d3ea5418d19dd07d77f760b9b9a078
Mickael Araujo <araujomickael18@gmail.com>
2020-06-05 20:31:14 -0300

    display map

--- frontend/src/pages/Point/index.tsx
+++ frontend/src/pages/Point/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 import logo from '../../assets/logo.svg'
 
@@ -66,6 +67,15 @@ const Point = () => {
 
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-7.0084946, -37.2651076]} zoom={15}>
+            <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-7.0084946, -37.2651076]} />
+          </Map>
 
           <div className="field-group">
             <div className="field">
